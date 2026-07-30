cda90f083055adb02b0c2fb86331cb73876204aa
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-30 23:20:20 +0200

    feat: new end point api/hello to test the whole pipeline works.

---
+++ src/main/java/com/quizarena/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.quizarena.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public String hello(){
+        return "Hello, world!\nQuiz Arena is Running.";
+    }
+}
