2414c003afa698467fa8ccc1ce54a155a1892815
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-30 22:53:55 +0200

    feat: initial commit of project Quiz Arena

---
+++ src/main/java/com/quizarena/QuizarenaApplication.java
@@ -0,0 +1,13 @@
+package com.quizarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizarenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizarenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quizarena/QuizarenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizarena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizarenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
