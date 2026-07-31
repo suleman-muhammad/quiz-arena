44f9d10bb33e7728e2bbda9b6abfa4e7c5b992b5
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-08-01 00:44:32 +0200

    feat: created a post end point for quiz creation in the QuizController class.

--- src/main/java/com/quizarena/controller/QuizController.java
+++ src/main/java/com/quizarena/controller/QuizController.java
@@ -1,10 +1,36 @@
 package com.quizarena.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.quizarena.entity.Question;
+import com.quizarena.entity.Quiz;
+import com.quizarena.repository.QuizRepository;
 
 @RestController
 @RequestMapping("/api/quizzes")
 public class QuizController {
+    
+    private final QuizRepository quizRepository;
+
+    public QuizController(QuizRepository quizRepository){
+        this.quizRepository = quizRepository;
+    }
+
+
+    @PostMapping
+    public Quiz creatQuiz(@RequestBody Quiz quiz){
+        
+        for (Question q: quiz.getQuestions()){
+            q.setQuiz(quiz);
+        }
+
+        quiz = quizRepository.saveAndFlush(quiz);
+        return quiz;
+    }
+
     
 }
