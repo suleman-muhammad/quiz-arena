62df237b2887bcf0bf48efb67f193d38c3057ba9
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-08-01 01:00:30 +0200

    feat: created a del end point to del an id specific quiz if it exists in the QuizController class.

--- src/main/java/com/quizarena/controller/QuizController.java
+++ src/main/java/com/quizarena/controller/QuizController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,5 +56,14 @@ public class QuizController {
         }
     }
 
-    
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Quiz> delteQuiz(@PathVariable Long id){
+        if(quizRepository.existsById(id)){
+            quizRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
