d07a83ec77c7d2e49380e6f8d59911a206d9f7ef
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-08-07 17:03:53 +0200

    feat: new DTO for Game start request.

---
+++ src/main/java/com/quizarena/dto/StartRoomRequest.java
@@ -0,0 +1,15 @@
+package com.quizarena.dto;
+
+public class StartRoomRequest {
+    private String roomCode;
+
+    public String getRoomCode() {
+        return roomCode;
+    }
+
+    public void setRoomCode(String roomCode) {
+        this.roomCode = roomCode;
+    }
+
+    
+}
