5d29aa07dc896434fb086dc47a31937554ffdcc1
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-08-07 17:06:00 +0200

    feat: new class that implements Comaprator to sort players according to their scores after each round.

--- src/main/java/com/quizarena/game/ComparePlayersForPosition.java
+++ src/main/java/com/quizarena/game/ComparePlayersForPosition.java
@@ -6,6 +6,6 @@ public class ComparePlayersForPosition implements Comparator<Player>{
     
     @Override
     public int compare(Player p1,Player p2){
-        return (int) (p1.getScore() - p2.getScore());
+        return (int) (p2.getScore() - p1.getScore());
     }
 }
