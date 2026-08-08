4ec6f8ab518fce34ea28eb160907fac84eb2e2ac
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-08-08 02:44:51 +0200

    refactor: changed the questions fetch type to eager in quiz entity.

--- src/main/java/com/quizarena/entity/Quiz.java
+++ src/main/java/com/quizarena/entity/Quiz.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -35,7 +36,7 @@ public class Quiz {
     // to stop refrencing back to back between quiz and questions.
     @JsonManagedReference 
     //for relation ship with questions, and remove quistons if parent quiz is deleted.  
-    @OneToMany(mappedBy = "quiz",cascade = CascadeType.ALL, orphanRemoval = true) 
+    @OneToMany(mappedBy = "quiz",cascade = CascadeType.ALL, orphanRemoval = true,fetch = FetchType.EAGER) 
     private List<Question> questions = new ArrayList<>();
 
     public Quiz() {
