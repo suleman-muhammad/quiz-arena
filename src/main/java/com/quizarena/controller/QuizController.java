58665a2c9e37a07a4f264356d6c0d42baaadc42a
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-08-10 13:27:31 +0200

    refactor: moved all the logic to quizService controller just now call the lower layers method and return result.

--- src/main/java/com/quizarena/controller/QuizController.java
+++ src/main/java/com/quizarena/controller/QuizController.java
@@ -1,7 +1,6 @@
 package com.quizarena.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,67 +11,50 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.quizarena.entity.Question;
 import com.quizarena.entity.Quiz;
-import com.quizarena.repository.QuizRepository;
+import com.quizarena.service.QuizService;
 
 @RestController
 @RequestMapping("/api/quizzes") // this class handles this end point and sub end points of it -if any
 public class QuizController {
     
-    // this is a bean
-    private final QuizRepository quizRepository;
+    private final QuizService quizService;
+    
 
-
-    public QuizController(QuizRepository quizRepository){
-        this.quizRepository = quizRepository;
+    public QuizController(QuizService quizService){
+        this.quizService = quizService;
     }
+
+
+    
 
 
     // this method handles the Post mapping on the class' original url
     @PostMapping
     public Quiz creatQuiz(@RequestBody Quiz quiz){ // request body handles the conversion of json to Quiz and quiz to Json while returning
         
-        // foreign key does not gets attached 
-        // cause only quiz gets serialized 
-        // so we have to set the quiz_id field in Questions manually
-        for (Question q: quiz.getQuestions()){
-            q.setQuiz(quiz);
-        }
-
-        quiz = quizRepository.saveAndFlush(quiz);
-        return quiz;
+        return quizService.creatQuiz(quiz);
     }
 
     // get mapping on main url
     // returns all quizzes
     @GetMapping
     public List<Quiz> getAllQuizzes(){
-        return quizRepository.findAll();
+        return quizService.getAllQuizzes();
     }
 
     //get mapping with main + sub url
     // @PathVariable takes the id from the url and assigns it to id
     @GetMapping("/{id}")
     public ResponseEntity<Quiz> quizWithId(@PathVariable Long id){
-        Optional<Quiz> quiz = quizRepository.findById(id);
-        
-        if(quiz.isPresent()){
-            return ResponseEntity.ok(quiz.get());
-        }else{
-            return ResponseEntity.notFound().build(); // 404 error code
-        }
+        return quizService.findQuizWithId(id);
     }
 
 
     // delete mapping with main + sub url
     @DeleteMapping("/{id}")
     public ResponseEntity<Quiz> delteQuiz(@PathVariable Long id){
-        if(quizRepository.existsById(id)){
-            quizRepository.deleteById(id);
-            return ResponseEntity.noContent().build(); // returns 204 cause nothing to show.
-        }
-        return ResponseEntity.notFound().build(); // returns 404 
+        return quizService.deleteQuizById(id);
     }
 
 }
