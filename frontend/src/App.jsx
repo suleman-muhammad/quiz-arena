26f0e78c2f89338bda03a5e0240df9773c662a1b
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-08-17 03:00:29 +0200

    feat: added path Route of Room.jsx in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Explore from './pages/Explore'
 import CreateQuiz from './pages/CreateQuiz'
 import JoinGame from './pages/JoinGame'
 import WaitingRoom from './pages/WaitingRoom'
+import Room from './pages/Room'
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/create" element={<CreateQuiz />} />
                 <Route path="/join" element={<JoinGame />} />
                 <Route path='/waitingRoom/:roomCode' element={<WaitingRoom />} />
+                <Route path='/room/:roomCode' element={<Room />} />
             </Routes>
         </BrowserRouter>
     )
