b704c58995f68d6e66cddcce3fc33e49c6816370
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-08-17 02:17:34 +0200

    feat: fetched info regarding quiz and players list using http.

--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -20,7 +20,50 @@ function Room(){
     const [questionCount, setQuestionCount] = useState(0)
 
 
-    
+    useEffect(() => {
+        fetch(`http://localhost:8080/api/rooms/${roomCode}`)
+            .then(res => res.json())
+            .then((data) => {
+                if(data === null){
+                    navigate("/")
+                }else{
+                    setPlayers(data.players)
+                }        
+            })
+            .catch(err => console.log(err))
+        
+        fetch(`http://localhost:8080/api/rooms/${roomCode}/quiz`)
+            .then(res => {
+                if(!res.ok){
+                    return null
+                }
+                return res.json()
+            })
+            .then((quizId) => {
+                if(quizId == null){
+                    console.log(`Room with code ${roomCode} does not have quiz.`);
+                    navigate("/")
+                }
+                
+                fetch(`http://localhost:8080/api/quizzes/${quizId}`)
+                .then(res => {
+                    if(!res.ok){
+                        return null
+                    }
+                    return res.json()
+                })
+                .then((data) => {
+                    if(data == null){
+                        console.log(`No quiz found with Id ${quizId}`)
+                        navigate("/")
+                    }
+                    setQuizId(quizId)
+                    setQuizTitle(data.title)
+                    setQuizDescription(data.description)
+                    setQuestionCount(data.questions.length)
+                })
+            })
+    },[])
 
     
 
