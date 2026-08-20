2371e5d38a5c154b921e5594cbe0e53bd8cec466
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-08-20 14:19:12 +0200

    refactor: changed StopAcceptingAnswerDTO to have a correctOption field as well. and made it a record

--- src/main/java/com/quizarena/dto/StopAcceptingAnswers.java
+++ src/main/java/com/quizarena/dto/StopAcceptingAnswers.java
@@ -1,22 +1,4 @@
 package com.quizarena.dto;
 
-public class StopAcceptingAnswers {
-    
-    private int questionNo;
-    private boolean isAccepting;
-    
-    public int getQuestionNo() {
-        return questionNo;
-    }
-    public void setQuestionNo(int questionNo) {
-        this.questionNo = questionNo;
-    }
-    public boolean isAccepting() {
-        return isAccepting;
-    }
-    public void setAccepting(boolean isAccepting) {
-        this.isAccepting = isAccepting;
-    }
-
-    
-}
+public record StopAcceptingAnswers(int questionNo, int correctOption) {
+} 
