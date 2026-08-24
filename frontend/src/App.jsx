8906797684f4bd84bf2a0d47ecbd8b88f260b1de
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-08-24 02:23:57 +0200

    feat: updated the overall background of the Application to be purple..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,17 +9,19 @@ import Room from './pages/Room'
 
 function App() {
     return (
-        <BrowserRouter>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/explore" element={<Explore />} />
-                <Route path="/create" element={<CreateQuiz />} />
-                <Route path="/join" element={<JoinGame />} />
-                <Route path='/waitingRoom/:roomCode' element={<WaitingRoom />} />
-                <Route path='/room/:roomCode' element={<Room />} />
-            </Routes>
-        </BrowserRouter>
+        <div className="min-h-screen bg-slate-950 text-slate-100 font-sans">
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/explore" element={<Explore />} />
+                    <Route path="/create" element={<CreateQuiz />} />
+                    <Route path="/join" element={<JoinGame />} />
+                    <Route path='/waitingRoom/:roomCode' element={<WaitingRoom />} />
+                    <Route path='/room/:roomCode' element={<Room />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
     )
 }
 
