92543735a2e007cae8fe68657c9fdb9f3a977246
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-08-28 05:02:55 +0200

    update: Trivia facts now have new required Fields.

--- src/main/java/com/quizarena/entity/TriviaFact.java
+++ src/main/java/com/quizarena/entity/TriviaFact.java
@@ -1,8 +1,60 @@
 package com.quizarena.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class TriviaFact {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+    private String icon;
+    private String title;
+    private String text;
+
+    // dont serialize this side.
+    // for stop refrencing back to back between quiz and questions.
+    @JsonBackReference
+    // relationship to quiz table. 
+    @ManyToOne(fetch = FetchType.LAZY)
+    // which column to join on.
+    @JoinColumn(name = "quiz_id")
+    private Quiz quiz;
+
+
+    
+    public String getIcon() {
+        return icon;
+    }
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+    public int getId() {
+        return id;
+    }
+
+
+    
 }
