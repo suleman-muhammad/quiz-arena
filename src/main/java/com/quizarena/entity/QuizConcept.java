2630c9211ddd373f20a4ba491129c81fb26e65ac
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-08-29 17:12:05 +0200

    feat: new Entity to store Quiz Concepts.

---
+++ src/main/java/com/quizarena/entity/QuizConcept.java
@@ -0,0 +1,46 @@
+package com.quizarena.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class QuizConcept {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    private String concept;
+    
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "quiz_id")
+    private Quiz quiz;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getConcept() {
+        return concept;
+    }
+
+    public void setConcept(String concept) {
+        this.concept = concept;
+    }
+
+    public Quiz getQuiz() {
+        return quiz;
+    }
+    public void setQuiz(Quiz quiz){
+        this.quiz = quiz;
+    }
+
+}
