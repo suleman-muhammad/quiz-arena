9e5665a5d03cc3708ce9b127eda52f56be14a546
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-09-09 13:37:13 +0200

    update(config): updated configuration to allow allowed origings dynamically

--- src/main/java/com/quizarena/config/CorsConfig.java
+++ src/main/java/com/quizarena/config/CorsConfig.java
@@ -3,13 +3,21 @@ package com.quizarena.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Override
+
+    @Value("${app.cors.allowed-origins:http://localhost:5173}")
+    private String allowedOrigins;
+
+
+     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/**")
+                .allowedOriginPatterns(allowedOrigins.split(","))
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
